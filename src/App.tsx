2dba64781a44cb3a3cab5fbbde2f173f6722cbc8
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-10-02 19:06:51 +0900

    [追加]eslintの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './App.css'
-
-function App() {
-  return (
-    <div>
-      <h1>Hello </h1>
-    </div>
-  )
-}
-
-export default App
+export const App = () => (
+  <div>
+    <h1>Hello </h1>
+  </div>
+)
